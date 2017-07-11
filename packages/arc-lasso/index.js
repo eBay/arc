669abed53ffd834a8f2a66ca084246218f960ddb
Michael Rawlings <ml.rawlings@gmail.com>
2017-07-11 14:56:21 -0700

    adapt .arc files for lasso-plugin (still need to do other resources)

--- packages/arc-lasso/index.js
+++ packages/arc-lasso/index.js
@@ -2,7 +2,21 @@ var path = require('path');
 var arcResolver = require('arc-resolver');
 
 module.exports = function(lasso, config) {
+    lasso.config.resolver = {
+        postResolve: function(resolvedInfo, lassoContext) {
+            var original = resolvedInfo.path;
+            if (/\.arc$/.test(original)) {
+                resolvedInfo.path = arcResolver.adaptResource(resolvedInfo.path, lassoContext.flags.flagMap);
+            }
+            if (original != resolvedInfo.path) {
+                delete resolvedInfo.meta;
+                console.log(original, '→', path.relative(path.dirname(original), resolvedInfo.path));
+            }
+        }
+    }
     lasso.dependencies.registerRequireType('arc', {
-        read: function(){}
+        read: function() {
+            return 'throw new Error("Something went wrong with the arc-lasso plugin.  The read function should not be called");';
+        }
     });
 }
