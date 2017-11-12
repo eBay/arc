83f677a8f5f1343bde4a202df9464ad29ba6f440
Michael Rawlings <ml.rawlings@gmail.com>
2017-11-11 19:21:33 -0800

    update tests to bracket syntax

--- packages/arc-fs/test.js
+++ packages/arc-fs/test.js
@@ -6,7 +6,7 @@ describe('AdaptiveFS', () => {
   it('reads adapted file', () => {
     let mfs = new MemoryFS();
     let afs = new AdaptiveFS({ fs: mfs, flags: () => ({ test: true }) });
-    mfs.writeFileSync('/file.test.js', 'Test Contents');
+    mfs.writeFileSync('/file[test].js', 'Test Contents');
     expect(afs.readFileSync('/file.js', 'utf-8')).to.equal('Test Contents');
   });
   it('fallsback to the node filesystem', () => {
