be9c5c17c5e63e9961bd30e98bf83c51e65b4bc8
Michael Rawlings <ml.rawlings@gmail.com>
2017-11-12 18:00:00 -0800

    add missing tests

--- packages/arc-fs/test.js
+++ packages/arc-fs/test.js
@@ -9,6 +9,16 @@ describe('AdaptiveFS', () => {
     mfs.writeFileSync('/file[test].js', 'Test Contents');
     expect(afs.readFileSync('/file.js', 'utf-8')).to.equal('Test Contents');
   });
+  it('resolves adapted file', () => {
+    let mfs = new MemoryFS();
+    let afs = new AdaptiveFS({ fs: mfs, flags: () => ({ test: true }) });
+    mfs.writeFileSync('/file[test].js', 'Test Contents');
+    expect(afs.resolveSync('/file.js')).to.equal('/file[test].js');
+  });
+  it('has a method to clear the resolver cache', () => {
+    let afs = new AdaptiveFS({ flags: () => ({ test: true }) });
+    afs.clearCache();
+  });
   it('fallsback to the node filesystem', () => {
     let afs = new AdaptiveFS({ flags: () => ({}) });
     expect(afs.existsSync(__filename)).to.equal(true);
