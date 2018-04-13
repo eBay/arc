d119de61c298dea28b424d02961f54ad5e577b07
Michael Rawlings <ml.rawlings@gmail.com>
2018-04-12 17:50:19 -0700

    add marko/lasso example

---
+++ packages/example-marko-lasso/src/server.js
@@ -0,0 +1,49 @@
+require('arc-server/install');
+require('marko/node-require').install({
+  compilerOptions: {
+    requireTemplates: true
+  }  
+});
+
+let arc = require('arc-server');
+let express = require('express');
+let lasso = require('lasso');
+let markoMiddleware = require('marko/express');
+let lassoMiddleware = require('lasso/middleware');
+let useragent = require('express-useragent');
+let template = require('./index.marko');
+let app = express();
+
+lasso.configure({
+    plugins: [
+        'arc-lasso',
+        'lasso-marko'
+    ]
+});
+
+app.use(markoMiddleware());
+app.use(lassoMiddleware.serveStatic());
+app.use(useragent.express());
+
+app.use((req, res, next) => {
+    arc.beginContext(() => {
+        let ua = req.useragent;
+        let flags = {
+            mobile: ua.isMobile,
+            desktop: ua.isDesktop,
+            microsoft: /microsoft|windows/i.test(ua.platform),
+            google: /google|android/i.test(ua.platform),
+            apple: /apple|ip(hone|ad|od)|mac/i.test(ua.platform)
+        }
+        arc.setFlags(flags);
+        res.locals.flags = Object.keys(flags).filter(flag => flags[flag]);
+        next();
+    });
+});
+
+app.get('/', (req, res) => {
+    console.log(res.locals.flags)
+  res.marko(template);
+});
+
+app.listen(8080);
