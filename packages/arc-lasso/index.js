b9c6db2aa3258a339242dd79fbe4ecf6b5769ab4
aganeshalingam <aganeshalingam@users.noreply.github.com>
2019-11-06 16:48:10 -0800

    Avoid openning AdaptiveFS for each request (#7)

--- packages/arc-lasso/index.js
+++ packages/arc-lasso/index.js
@@ -1,10 +1,18 @@
 let path = require('path');
 let AdaptiveFS = require('arc-fs');
+let fsCache = new WeakMap();
 
 module.exports = function(lasso, config) {
     lasso.config.resolver = {
         postResolve: function(resolvedInfo, lassoContext) {
-            let afs = new AdaptiveFS({ flags: lassoContext.flags.flagMap });
+            let flags = lassoContext.flags.flagMap;
+            let afs = fsCache.get(flags);
+
+            if (!afs) {
+                afs = new AdaptiveFS({ flags });
+                fsCache.set(flags, afs);
+            }
+
             let originalPath = resolvedInfo.path;
             let adaptedPath = resolvedInfo.path = afs.resolveSync(originalPath);
 
